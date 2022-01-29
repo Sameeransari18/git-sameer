67a68816115affc520f97eb7b61df81630d838e6
admin 1 <admin 1@DESKTOP-5G7O8IS>
2022-01-29 19:18:28 +0530

    second

---
+++ Firnas/src/addition.java
@@ -0,0 +1,17 @@
+
+public class addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 10;
+		int b = 20;
+		
+		System.out.println("the value of a is..." + a);
+		System.out.println("the value of b is..." + b);
+		
+		int c = a+b;
+		System.out.println();
+		System.out.println("Addition of two values is..." + c);
+	}
+
+}
