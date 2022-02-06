0eb5426c61558d9ca49ff930eb059c6cf0b37ade
admin 1 <admin 1@DESKTOP-5G7O8IS>
2022-02-06 12:27:43 +0530

    multiplication

---
+++ Firnas/src/Firnas/multiplication.java
@@ -0,0 +1,10 @@
+package Firnas;
+
+public class multiplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("multiplication");
+	}
+
+}
