ca1f550af6a75570ae40911474be28237f6f4546
admin 1 <admin 1@DESKTOP-5G7O8IS>
2022-04-09 12:42:34 +0530

    type casting1

--- Firnas/src/Firnas/typeCasting.java
+++ Firnas/src/Firnas/typeCasting.java
@@ -1,9 +1,14 @@
 package Firnas;
 
+import java.awt.Window.Type;
+
 public class typeCasting {
 	public static void main(String[] args) {
 		int i = 10;
-		Integer ii = i;
+		Integer ii = i; //auto boxing
+		
+		i = ii;	//auto unboxing 
+		
 		
 		System.out.println(ii);
 	}
